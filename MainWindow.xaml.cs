acde7d53593ee83e5ef2249d25a30d85eb445e2a
Anishko <anishkokoneko04@gmail.com>
2025-07-07 23:28:31 +0300

    23:28 07.07.2025

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,26 +17,5 @@ namespace MySleepHelperApp
         {
             InitializeComponent();
         }
-
-        private double aspectRatio = 4.0 / 3.0; // Соотношение сторон 4:3
-
-        private void Grid_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            // Получаем текущие размеры окна
-            double currentWidth = this.Width;
-            double currentHeight = this.Height;
-
-            // Вычисляем новую ширину/высоту, чтобы сохранить пропорции
-            if (currentWidth / currentHeight > aspectRatio)
-            {
-                // Ограничение: ширина не должна выходить за рамки пропорций
-                this.Width = currentHeight * aspectRatio;
-            }
-            else
-            {
-                // Ограничение: высота не должна выходить за рамки пропорций
-                this.Height = currentWidth / aspectRatio;
-            }
-        }
     }
 }
