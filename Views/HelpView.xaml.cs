ee1b889ad6156c19b2784e96d3a369959d4efaff
Anishko <anishkokoneko04@gmail.com>
2025-08-03 05:27:24 +0300

    Доделан XAML вкладки "Справка" + 2 кнопки.

--- Views/HelpView.xaml.cs
+++ Views/HelpView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
 using System.Windows.Controls;
 
 
@@ -9,6 +10,74 @@ namespace MySleepHelperApp.Views
         public HelpView()
         {
             InitializeComponent();
+        }
+
+        private void HelpButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Сначала спрашиваем пользователя
+            var result = CustomMessageBox.ShowYesNoDialog(
+                "Вы будете перенаправлены на страницу GitHub для создания обращения. Продолжить?",
+                "Переход к GitHub",
+                Window.GetWindow(this)
+            );
+
+            // Если пользователь нажал "Да"
+            if (result == true)
+            {
+                try
+                {
+                    // Открываем страницу создания нового issue
+                    Process.Start(new ProcessStartInfo
+                    {
+                        FileName = "https://github.com/chth-dev/MySleepHelperApp/issues/new/choose",
+                        UseShellExecute = true
+                    });
+                }
+                catch
+                {
+                    // Если не удалось открыть браузер - показываем ошибку
+                    CustomMessageBox.ShowDialog(
+                        $"Не удалось открыть браузер :(",
+                        "Что-то пошло не так",
+                        Window.GetWindow(this)
+                    );
+                }
+            }
+            // Если пользователь нажал "Нет" - просто закрываем диалог, ничего дополнительно делать не нужно
+        }
+
+        private void GitButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Сначала спрашиваем пользователя
+            var result = CustomMessageBox.ShowYesNoDialog(
+                "Вы будете перенаправлены на страницу GitHub проекта. Продолжить?",
+                "Переход к GitHub",
+                Window.GetWindow(this)
+            );
+
+            // Если пользователь нажал "Да"
+            if (result == true)
+            {
+                try
+                {
+                    // Открываем страницу вашего GitHub репозитория
+                    Process.Start(new ProcessStartInfo
+                    {
+                        FileName = "https://github.com/chth-dev/MySleepHelperApp",
+                        UseShellExecute = true
+                    });
+                }
+                catch
+                {
+                    // Если не удалось открыть браузер - показываем ошибку
+                    CustomMessageBox.ShowDialog(
+                        $"Не удалось открыть браузер :(",
+                        "Что-то пошло не так",
+                        Window.GetWindow(this)
+                    );
+                }
+            }
+            // Если пользователь нажал "Нет" - просто закрываем диалог
         }
     }
 }
